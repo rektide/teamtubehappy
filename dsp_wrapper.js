7ac2b48ff26dbd17be148cd8f82a3e1098e41a04
rektide <rektide@voodoowarez.com>
2010-08-29 18:05:18 -0400

    Fix up dsp_wrapper by enumerating on GLOBAL.

--- dsp_wrapper.js
+++ dsp_wrapper.js
@@ -1,12 +1,12 @@
 var fs = require("fs")
 
-var oldroot = []
-for(var i in this)
-	oldroot = i
+var oldroot = {}
+for(var i in GLOBAL)
+	oldroot[i] = true
 
-var dspjs = fs.readFileSync("./lib/dsp.js/dsp.js")
+var dspjs = fs.readFileSync("./lib/dsp.js/dsp.js","ascii")
 eval(dspjs)
 
-for(var i in this)
+for(var i in GLOBAL)
 	if(!oldroot[i])
-		exports[i] = this[i]
+		exports[i] = GLOBAL[i]
